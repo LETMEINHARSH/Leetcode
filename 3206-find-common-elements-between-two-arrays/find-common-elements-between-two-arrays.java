20566f2edc0c2bcd73ea0a41a7903ca80fddf893
Harsh Amarsingh Rajput <166542364+LETMEINHARSH@users.noreply.github.com>
2026-07-20 18:43:48 +0530

    Time: 7 ms (29.28%) | Memory: 47.2 MB (88.13%) - LeetSync

---
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -0,0 +1,26 @@
+//Time  : O(n + m)
+//Space : O(n + m)
+
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for(int i =0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        int count2 = 0;
+        for(int i = 0;i<nums2.length;i++){
+            if(set1.contains(nums2[i])){
+                count2++;
+            }
+            set2.add(nums2[i]);
+        }
+        int count1 = 0;
+        for(int i=0;i<nums1.length;i++){
+            if(set2.contains(nums1[i])){
+                count1++;
+            }
+        }
+        return new int[] {count1,count2};
+    }
+}
