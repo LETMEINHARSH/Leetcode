97caa5937e5c00f7c37b20330b1dbd43cf060048
Harsh Amarsingh Rajput <166542364+LETMEINHARSH@users.noreply.github.com>
2026-07-23 14:54:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.76%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        int dp[] =new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<dp.length;i++){
+            int rob = dp[i-2] + nums[i];
+            int dontrob = dp[i-1];
+            dp[i] = Math.max(rob,dontrob);
+        }
+        return dp[nums.length - 1];
+    }
+}
